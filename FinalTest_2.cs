12c473dc3bb72f265a84e5b42950d74f84469929
ratchathorn2545 <78552921+ratchathorn2545@users.noreply.github.com>
2021-06-13 16:48:10 +0700

    Add files via upload

---
+++ FinalTest_2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Final_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int n = 1;
+            int[] a = new int[100];
+            Console.Write("Put Number: ");
+
+            for (i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n - 1; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        int number = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = number;
+                    }
+                }
+            }
+            Console.Write("GG' s Sort : ");
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write(a[i] + " ");
+                Console.ReadLine();
+            }
+        }
+    }
+}
